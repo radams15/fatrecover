087e4309b16cfd7d35b297cd69b3d47b1bc2858a
Rhys Adams <rhys@therhys.co.uk>
2024-03-19 15:08:18 +0000

    Large improvements, now reads the FAT.

--- src/main.pas
+++ src/main.pas
@@ -30,34 +30,82 @@ type
         fileSize: uint32;
     end;
 
-    procedure ReadFile(fileName: String);
+    Cluster = Array[0..127] of uint32;
+    ClusterFile = File of Cluster;
+    DirEntFile = File of DirEnt;
+
+    procedure ReadClusterChain(dirFile: DirEnt ; fat: Cluster);
     var
-        imgFile: File of DirEnt;
-        rootDir: DirEnt;
-        name: String;
+        fatVal: uint32;
         i: uint32;
     begin
-        AssignFile(imgFile, fileName);
+        for i:=0 to 16 do
+        begin
+            fatVal := fat[i] AND $0FFFFFFF;
+            WriteLn(Format('%x => %x', [i, fatVal]));
+        end;
+    end;
+
+    procedure PrintDirEnt(var imgFile: DirEntFile ; fat: Cluster);
+    var
+        rootDir: DirEnt;
+        name: String;
+    begin
+        Read(imgFile, rootDir);
+
+        if rootDir.fullName[0] = DeletedByte then
+        begin
+            //Write('Deleted: ');
+            rootDir.fullName[0] := Byte('_');
+        end;
+
+        name := StrPas(@rootDir.fullName);
+
+        if length(name) = 0 then
+        begin
+            Exit;
+        end;
+
+        WriteLn(Format('%s => %x bytes @ cluster %x', [name, rootDir.fileSize, rootDir.cluster]));
+
+        ReadClusterChain(rootDir, fat);
+    end;
+
+    function ReadFat(var imgFile: ClusterFile) : Cluster;
+    var
+        fat: Cluster;
+
+    begin
         Reset(imgFile);
-        Seek(imgFile, $10200);
+        Seek(imgFile, $20);
+
+        Read(imgFile, fat);
+
+        ReadFat := fat;
+    end;
+
+    procedure ReadDir(var dirFile: DirEntFile ; fat: Cluster);
+    var
+        i: uint32;
+    begin
+        Reset(dirFile);
+        Seek(dirFile, $10200);
 
         for i := 0 to 32 do
         begin
-            Read(imgFile, rootDir);
-
-            if rootDir.fullName[0] = DeletedByte then
-            begin
-                Write('Deleted: ');
-                rootDir.fullName[0] := Byte('_');
-            end;
-
-            name := StrPas(@rootDir.fullName);
-
-            if length(name) <> 0 then
-            begin
-                WriteLn(Format('%s => %x bytes @ cluster %x', [name, rootDir.fileSize, rootDir.cluster]));
-            end;
+            PrintDirEnt(dirFile, fat);
         end;
+    end;
+
+    procedure ReadFile(fileName: String);
+    var
+        imgFile: ClusterFile;
+        fat: Cluster;
+    begin
+        AssignFile(imgFile, fileName);
+
+        fat := ReadFat(imgFile);
+        ReadDir(DirEntFile(imgFile), fat);
     end;
 
 begin
